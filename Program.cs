970527621cf07083d84cb803156ec73dbc8ead02
andrewmihalkov228 <andrewmihalkov228@gmail.com>
2024-04-24 14:18:32 +0400

    plus

--- Program.cs
+++ Program.cs
@@ -19,29 +19,6 @@
         {
             return a + b;
         }
-
-        public double Subtract()
-        {
-            return a - b;
-        }
-
-        public double Multiply()
-        {
-            return a * b;
-        }
-
-        public double Divide()
-        {
-            if (b != 0)
-            {
-                return a / b;
-            }
-            else
-            {
-                Console.WriteLine("Ошибка: деление на ноль!");
-                return 0;
-            }
-        }
     }
 
     class Program
@@ -56,9 +33,6 @@
                 Console.WriteLine("1. Ввести А");
                 Console.WriteLine("2. Ввести В");
                 Console.WriteLine("3. Выполнить операцию \"+\"");
-                Console.WriteLine("4. Выполнить операцию \"-\"");
-                Console.WriteLine("5. Выполнить операцию \"*\"");
-                Console.WriteLine("6. Выполнить операцию \"/\"");
                 Console.WriteLine("0. Выход");
 
                 Console.Write("Выберите пункт меню: ");
@@ -79,18 +53,6 @@
                     case "3":
                         double sum = calculator.Add();
                         Console.WriteLine("Результат: " + sum);
-                        break;
-                    case "4":
-                        double difference = calculator.Subtract();
-                        Console.WriteLine("Результат: " + difference);
-                        break;
-                    case "5":
-                        double product = calculator.Multiply();
-                        Console.WriteLine("Результат: " + product);
-                        break;
-                    case "6":
-                        double quotient = calculator.Divide();
-                        Console.WriteLine("Результат: " + quotient);
                         break;
                     case "0":
                         Console.WriteLine("Выход из программы.");
